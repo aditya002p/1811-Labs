b9e227aacd93e41bf38d9314644167f80d9b7ed5
Aditya <ricky.pathak902@gmail.com>
2024-07-30 00:23:25 +0530

    commiting for 1811 labs

--- src/components/Landing-page-10/Footer.tsx
+++ src/components/Landing-page-10/Footer.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
 import image from "@/assets/img_footer_logo.png";
+import { Inter } from "next/font/google";
 
 const options = {
   all: [
@@ -25,12 +26,12 @@ const options = {
 
 export default function Footer() {
   return (
-    <div className="md:py-20 md:pb-28 md:px-[135px] px-8 py-10">
+    <div className="bg-black md:py-20 md:pb-28 md:px-[135px] px-8 py-10 bg-gradient-to-b from-[#090A18] from-80% to-[#313B827D]">
       <div className="flex flex-col md:gap-16 gap-8">
-        <div className="flex justify-between flex-col md:flex-row gap-10 md:gap-0">
-          <div className="flex flex-col gap-8 max-w-96">
-            <Image src={image} alt="not found" width={150} height={150} />
-            <span className="text-[#B0B2B8] md:text-sm">
+        <div className="flex flex-col md:flex-row gap-6 md:gap-10">
+          <div className="flex flex-col gap-6 md:gap-8 max-w-96">
+            <Image src={image} alt="Logo" width={150} height={150} />
+            <span className="text-[#B0B2B8] md:text-sm text-base">
               Unleash Your Creative Potential by Turning What You Consume into
               Engaging Content Ideas
             </span>
@@ -40,13 +41,13 @@ export default function Footer() {
             </Button>
           </div>
 
-          <div className="grid ml-10 grid-cols-3 md:gap-x-[140px] gap-y-4 gap-x-20 text-[#B0B2B8] text-sm font-medium">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-10 text-[#B0B2B8] text-sm">
             <div className="flex flex-col">
-              <span className="text-white pb-5 text-sm font-medium">All</span>
+              <span className="text-white pb-3 text-sm font-medium">All</span>
               {options.all.map((option, index) => (
                 <Link
                   href={option.url}
-                  className="text-start text-lg "
+                  className="text-start text-lg hover:underline"
                   key={index}
                 >
                   {option.label}
@@ -54,11 +55,11 @@ export default function Footer() {
               ))}
             </div>
             <div className="flex flex-col">
-              <span className="text-white pb-5 font-medium ">Info</span>
+              <span className="text-white pb-3 font-medium">Info</span>
               {options.info.map((option, index) => (
                 <Link
                   href={option.url}
-                  className="text-start text-lg"
+                  className="text-start text-lg hover:underline"
                   key={index}
                 >
                   {option.label}
@@ -66,11 +67,11 @@ export default function Footer() {
               ))}
             </div>
             <div className="flex flex-col">
-              <span className="text-white pb-5 font-medium">Contact Us</span>
+              <span className="text-white pb-3 font-medium">Contact Us</span>
               {options.contact.map((option, index) => (
                 <Link
                   href={option.url}
-                  className="text-start text-lg"
+                  className="text-start text-lg hover:underline"
                   key={index}
                 >
                   {option.label}
@@ -80,7 +81,7 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="text-center mt-20">
+        <div className="text-center mt-10 md:mt-20">
           <span className="text-[#B0B2B8] text-sm font-medium">
             © 2024 - 1811 Labs
           </span>
--- src/components/Landing-page-10/index.tsx
+++ src/components/Landing-page-10/index.tsx
@@ -5,11 +5,11 @@ import Features from "./Features";
 import Pricing from "./Pricing";
 import Faq from "./FAQ";
 import Footer from "./Footer";
-
+import "@fontsource/inter";
 export default function LandingPage8() {
   return (
     <div className="text-[#090A18]">
-      <div className="bg-[#1e202d] bg-gradient-to-tr from-[#1e202d]] to-[#01020b5d] text-white">
+      <div className="bg-[#1e202d] bg-gradient-to-tr from-[#1e202d]] to-[#01020b5d] text-white font-inter">
         <Hero />
         <FeaturedOn />
         <Features2 />
